1de095f58d28c1118686fac93eeb8e6d87f56c01
Nikolay Korotkov <nikolayfrontendmain@gmail.com>
2025-01-21 18:53:03 +0300

    try 3

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -31,7 +31,10 @@ class Category(models.Model):
     slug = models.SlugField(
         unique=True,
         verbose_name='Идентификатор',
-        help_text=('Идентификатор страницы для URL; разрешены символы латиницы, цифры, дефис и подчёркивание.')
+        help_text=(
+            'Идентификатор страницы для URL; разрешены символы'
+            ' латиницы, цифры, дефис и подчёркивание.'
+        )
     )
     is_published = models.BooleanField(
         default=True,
@@ -56,7 +59,10 @@ class Post(models.Model):
     text = models.TextField(verbose_name='Текст')
     pub_date = models.DateTimeField(
         verbose_name='Дата и время публикации',
-        help_text=('Если установить дату и время в будущем — можно делать отложенные публикации.')
+        help_text=(
+            'Если установить дату и время в будущем — можно делать'
+            ' отложенные публикации.'
+        )
     )
     author = models.ForeignKey(
         User,
